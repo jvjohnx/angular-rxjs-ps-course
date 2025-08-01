91299833da80fa11264881af3f32b614336b87a1
johnson <johnson@jvjohn.org>
2025-08-01 19:34:44 -0400

    in product.service.ts, update productsResource to use rxResource to fetch data

--- apm-begin/src/app/products/product.service.ts
+++ apm-begin/src/app/products/product.service.ts
@@ -1,6 +1,7 @@
-import { httpResource } from '@angular/common/http';
-import { Injectable, signal } from '@angular/core';
+import {HttpClient, httpResource} from '@angular/common/http';
+import {inject, Injectable, signal} from '@angular/core';
 import { Product } from './product';
+import {rxResource} from '@angular/core/rxjs-interop';
 
 @Injectable({
   providedIn: 'root'
@@ -8,10 +9,17 @@ import { Product } from './product';
 export class ProductService {
   private productsUrl = 'api/products';
 
+  http = inject(HttpClient);
+
   // Signals to support the template
   selectedProduct = signal<Product | undefined>(undefined);
 
   // Retrieve data into a signal
-  productsResource = httpResource<Product[]>(() => this.productsUrl, { defaultValue: [] });
+  // productsResource = httpResource<Product[]>(() => this.productsUrl, { defaultValue: [] }); //using httpResource
+
+  //update  productsResource to use rxResource instead of httpResource
+  productsResource = rxResource({
+    stream:  () => this.http.get(this.productsUrl)
+  });
 
 }
